5114c57933398a6e9fe14ae9b60a78313b6e6870
Danilo Paz Guedes de Freitas <danilotiba@gmail.com>
2024-12-26 16:37:03 -0300

    added parsing of grouped expression

--- src/parser.rs
+++ src/parser.rs
@@ -57,6 +57,7 @@ impl Parser {
         parser.register_prefix(TokenKind::Minus, Self::parse_prefix_expression);
         parser.register_prefix(TokenKind::True, Self::parse_boolean);
         parser.register_prefix(TokenKind::False, Self::parse_boolean);
+        parser.register_prefix(TokenKind::LParen, Self::parse_grouped_expression);
 
         parser.register_infix(TokenKind::Plus, Self::parse_infix_expression);
         parser.register_infix(TokenKind::Minus, Self::parse_infix_expression);
@@ -299,6 +300,18 @@ impl Parser {
             token: self.cur_token.clone(),
             value: self.cur_token_is(TokenKind::True),
         }))
+    }
+
+    fn parse_grouped_expression(&mut self) -> Option<ExpressionNode> {
+        self.next_token();
+
+        let exp = self.parse_expression(PrecedenceLevel::Lowest);
+
+        if !self.expect_peek(TokenKind::RParen) {
+            return None;
+        }
+
+        exp
     }
 }
 
@@ -695,6 +708,11 @@ mod tests {
             ("false", "false"),
             ("3 > 5 == false", "((3 > 5) == false)"),
             ("3 < 5 == true", "((3 < 5) == true)"),
+            ("1 + (2 + 3) + 4", "((1 + (2 + 3)) + 4)"),
+            ("(5 + 5) * 2", "((5 + 5) * 2)"),
+            ("2 / (5 + 5)", "(2 / (5 + 5))"),
+            ("-(5 + 5)", "(-(5 + 5))"),
+            ("!(true == true)", "(!(true == true))"),
         ];
 
         for test in tests {
